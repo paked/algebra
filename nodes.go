a8e7b5595290c80c807c8268a9738fcc65c3b254
Harrison <harrison@theshoebridges.com>
2015-03-24 13:06:23 +1100

    +nodes: move Node definitions from parser into other file

---
+++ nodes.go
@@ -0,0 +1,45 @@
+package algebra
+
+type Node interface {
+	Eval() int
+}
+
+type NumberNode struct {
+	Number int
+}
+
+func (n NumberNode) Eval() int {
+	return n.Number
+}
+
+type AdditionNode struct {
+	Left, Right Node
+}
+
+func (a AdditionNode) Eval() int {
+	return a.Left.Eval() + a.Right.Eval()
+}
+
+type SubtractionNode struct {
+	Left, Right Node
+}
+
+func (s SubtractionNode) Eval() int {
+	return s.Left.Eval() - s.Right.Eval()
+}
+
+type MultiplicationNode struct {
+	Left, Right Node
+}
+
+func (m MultiplicationNode) Eval() int {
+	return m.Left.Eval() * m.Right.Eval()
+}
+
+type DivisionNode struct {
+	Left, Right Node
+}
+
+func (d DivisionNode) Eval() int {
+	return d.Left.Eval() / d.Right.Eval()
+}
